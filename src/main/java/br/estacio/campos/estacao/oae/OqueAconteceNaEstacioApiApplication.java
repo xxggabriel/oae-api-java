c56d8544cf6fabf7254864454c5a369f9917c72e
Gabriel Oliveira <gabriel@umobi.com.br>
2022-10-09 13:22:55 -0300

    first commit

---
+++ src/main/java/br/estacio/campos/estacao/oae/OqueAconteceNaEstacioApiApplication.java
@@ -0,0 +1,13 @@
+package br.estacio.campos.estacao.oae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OqueAconteceNaEstacioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OqueAconteceNaEstacioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/estacio/campos/estacao/oae/OqueAconteceNaEstacioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.estacio.campos.estacao.oae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OqueAconteceNaEstacioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
